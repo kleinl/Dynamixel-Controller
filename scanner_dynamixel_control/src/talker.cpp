02a7c9c6f33c9160a1b5275a93c44aa0736340a0
Lukas <klein.lukas@outlook.de>
2018-04-06 17:24:01 +0200

    update

--- scanner_dynamixel_control/src/talker.cpp
+++ scanner_dynamixel_control/src/talker.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/Float64.h"
 #include "std_msgs/String.h"
 #include "dynamixel_controllers/SetCompliancePunch.h"
+#include "dynamixel_controllers/SetComplianceSlope.h"
 #include <stdlib.h>
 
 ros::Publisher chatter_pub;
@@ -12,9 +13,10 @@ double factor = (1.0/(360.0/6.3));
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
     std_msgs::Float64 message;
-    message.data = counter*(degrees*factor);
+    double position_degrees = counter*degrees;
+    message.data = position_degrees*factor;
 
-    ROS_INFO("%f", message.data);
+    ROS_INFO("%f degrees", position_degrees);
 
     chatter_pub.publish(message);
     counter++;
@@ -36,10 +38,15 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  system("rosservice call /tilt_controller/set_compliance_punch 'punch: 20.0'");
-  system("rosservice call /tilt_controller/set_compliance_margin 'margin: 0.0'");
-  system("rosservice call /tilt_controller/set_compliance_slope 'slope: 128.0'");
-  system("rosservice call /tilt_controller/torque_enable 'torque_enable: 1.0'");
+  ros::ServiceClient client = n.serviceClient<dynamixel_controllers::SetCompliancePunch>("/dynamixel_controllers/set_compliance_punch");
+  dynamixel_controllers::SetCompliancePunch srv;
+  srv.request.punch = 15;
+  client.call(srv);
+
+  ros::ServiceClient client2 = n.serviceClient<dynamixel_controllers::SetComplianceSlope>("/dynamixel_controllers/set_compliance_slope");
+  dynamixel_controllers::SetComplianceSlope srv2;
+  srv2.request.slope = 64;
+  client2.call(srv2);
 
   chatter_pub = n.advertise<std_msgs::Float64>("tilt_controller/command", 1000);
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);

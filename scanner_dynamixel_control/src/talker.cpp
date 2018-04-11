05488c76e46ff651d247e09efe865ce01e213490
Lukas <klein.lukas@outlook.de>
2018-04-11 14:25:04 +0200

    motor setings in .yaml file insted of code

--- scanner_dynamixel_control/src/talker.cpp
+++ scanner_dynamixel_control/src/talker.cpp
@@ -1,23 +1,26 @@
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
 #include "std_msgs/String.h"
-#include "dynamixel_controllers/SetCompliancePunch.h"
-#include "dynamixel_controllers/SetComplianceSlope.h"
 #include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 ros::Publisher chatter_pub;
 int degrees;
 int counter = 0;
-double factor = (1.0/(360.0/6.3));
+// Factor to calculate from degrees to radians.
+double factor = (1.0/(180.0/M_PI));
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
     std_msgs::Float64 message;
+    // Calculate desired goal positon in radians.
     double position_degrees = counter*degrees;
     message.data = position_degrees*factor;
 
     ROS_INFO("%f degrees", position_degrees);
 
+    // Send desired goal position.
     chatter_pub.publish(message);
     counter++;
 
@@ -27,31 +30,22 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
-  if (argc != 2) {
-    ROS_WARN("WARNING: you should specify the degrees per step! Standard is set to 10.");
-    degrees = 10;
-  } else {
-    ROS_INFO("INFO: you have set the degrees per step to: %s\n",argv[1]);
-    degrees = atoi(argv[1]);
-  } 
+    ros::init(argc, argv, "talker");
+    if (argc != 2) {
+        ROS_WARN("WARNING: you should specify the degrees per step! Standard is set to 10.");
+        degrees = 10;
+    } else {
+        ROS_INFO("INFO: you have set the degrees per step to: %s\n",argv[1]);
+        degrees = atoi(argv[1]);
+    } 
+    ros::NodeHandle n;
+  
+    // Send messages to "tilt_controller/command" to set target position.
+    chatter_pub = n.advertise<std_msgs::Float64>("tilt_controller/command", 1000);
+    // Wait for "chatter" to receive a message, then execute chatterCallback
+    // routine
+    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 
-  ros::NodeHandle n;
-
-  ros::ServiceClient client = n.serviceClient<dynamixel_controllers::SetCompliancePunch>("/dynamixel_controllers/set_compliance_punch");
-  dynamixel_controllers::SetCompliancePunch srv;
-  srv.request.punch = 15;
-  client.call(srv);
-
-  ros::ServiceClient client2 = n.serviceClient<dynamixel_controllers::SetComplianceSlope>("/dynamixel_controllers/set_compliance_slope");
-  dynamixel_controllers::SetComplianceSlope srv2;
-  srv2.request.slope = 64;
-  client2.call(srv2);
-
-  chatter_pub = n.advertise<std_msgs::Float64>("tilt_controller/command", 1000);
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-
-  ros::spin();
-
-  return 0;
+    ros::spin();
+    return 0;
 }
